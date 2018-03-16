cb166e592d3e44405dd4a951ae197ebe30ff752a
chauhan-rahul <chauhan_rahul@live.com>
2018-03-16 15:37:47 +0530

    Added the Basic structure for Project

---
+++ index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// main app
+//import App from "./containers/App";
+import { Provider } from "react-redux";
+
+// main routes
+import AppRoutes from "./routes";
+
+import store from "./store";
+ReactDOM.render(
+    <Provider store={store}>
+        <AppRoutes />
+    </Provider>,
+    document.getElementById("app")
+)
